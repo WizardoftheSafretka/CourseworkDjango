57001bbb8960c969a6b6130f34224c57e8617282
Wizard <merihov2yandex.ru>
2026-04-30 19:33:21 +0300

    start working

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mailing',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -103,3 +105,4 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
